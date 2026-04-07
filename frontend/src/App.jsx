f34f73b1552d66d7d448dcbe90308ee52fa355d7
José Ednaldo De Oliveira Filho <joseesnaldo6@gmail.com>
2026-04-07 10:26:22 -0300

    Limpeza geral, retirando linhas inutilizadas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 import { Header } from './components/header/Header'
 import { Footer } from './components/footer/Footer'
--- frontend/src/pages/institucional/PgPrivacidade.jsx
+++ frontend/src/pages/institucional/PgPrivacidade.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from '../../styles/PgPrivacidade.module.css';
 
 export default function PgPrivacidade() {
